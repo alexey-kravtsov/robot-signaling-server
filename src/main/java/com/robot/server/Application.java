aaafb70ffa66b7d35e60cfa5efdddc5a6e036ab8
Alexey Kravtsov <Alexey.Kravtsov@infobip.com>
2020-03-03 19:46:57 +0300

    added server

---
+++ src/main/java/com/robot/server/Application.java
@@ -0,0 +1,14 @@
+package com.robot.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@SpringBootApplication
+@EnableWebSocketMessageBroker
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
